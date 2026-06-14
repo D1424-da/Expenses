b8bb0d5170470df5bdc33e2d8d82d203911d479c
Daisuke Ikeda <d.i.a.0101@gmail.con>
2026-06-14 17:50:42 +0900

    変更

--- static/firebase-config.js
+++ static/firebase-config.js
@@ -9,7 +9,7 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyDwEtOmYVP7bjmjnDsKOuAIWWJx6FFV2os",
-  authDomain: "expenses-9af61.firebaseapp.com",
+  authDomain: "expenses-9af61.web.app",
   projectId: "expenses-9af61",
   storageBucket: "expenses-9af61.firebasestorage.app",
   messagingSenderId: "277005147489",
